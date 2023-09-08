5c579cf0f044f5decf5b23515c6a181258534079
splinterzpm <marushov1998@mail.ru>
2023-09-08 23:26:13 +0500

    removing backend azurerm due to ci settings including this

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = var.resource_group_name
-    storage_account_name = "covtrackertfstatestorage"
-    container_name = "tfstate"
-    key = "terraform.tfstate"
-  }
-}
-
 provider "azurerm" {
   features {}
 }
